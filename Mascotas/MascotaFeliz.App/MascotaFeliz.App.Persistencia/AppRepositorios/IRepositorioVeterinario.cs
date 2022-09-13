4057cc6a88f00390ec5e6c5fcc474566645b6a73
Edwin Suesca <edwcarte16@gmail.com>
2022-09-12 19:01:02 -0500

    Se agrega proyecto MAscota Feliz

---
+++ Mascotas/MascotaFeliz.App/MascotaFeliz.App.Persistencia/AppRepositorios/IRepositorioVeterinario.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using MascotaFeliz.App.Dominio;
+
+namespace MascotaFeliz.App.Persistencia
+{
+    public interface IRepositorioVeterinario
+    {       
+        Veterinario AddVeterinario(Veterinario veterinario);
+    }
+}
